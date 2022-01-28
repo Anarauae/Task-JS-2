2014a1469ef4347bbde2216c098da47beb65d298
Anara Kulmambetova <anarauae@gmail.com>
2022-01-29 01:31:20 +0400

    Added result 12 in script4

--- script4.js
+++ script4.js
@@ -1,4 +1,4 @@
 // 4-Дана строка'I am a hero!'. Выведите в консоль количество символов в этой строке.
 let string = "I am a hero!";
 let result = string.length;
-console.log(result);
+console.log(result);     // => 12
